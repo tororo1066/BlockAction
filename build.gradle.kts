14b89b474c6b7c91f4254af9a1e97703ee18a0ea
tororo_1066 <71592738+tororo1066@users.noreply.github.com>
2023-12-16 06:44:03 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask.JarUrl
+import groovy.lang.Closure
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    id("dev.s7a.gradle.minecraft.server") version "1.2.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.jmailen.kotlinter") version "3.8.0"
+}
+
+val gitVersion: Closure<String> by extra
+
+val pluginVersion: String by project.ext
+val apiVersion: String by project.ext
+
+repositories {
+    mavenCentral()
+    maven(url = "https://oss.sonatype.org/content/groups/public/")
+    maven(url = "https://papermc.io/repo/repository/maven-public/")
+    maven(url = "https://libraries.minecraft.net")
+    maven(url = "https://jitpack.io")
+}
+
+val shadowImplementation: Configuration by configurations.creating
+configurations["implementation"].extendsFrom(shadowImplementation)
+
+dependencies {
+    compileOnly(kotlin("stdlib"))
+    compileOnly("io.papermc.paper:paper-api:$pluginVersion-R0.1-SNAPSHOT")
+    compileOnly("com.github.tororo1066.TororoPluginAPI:base:$apiVersion")
+    shadowImplementation("com.github.tororo1066.TororoPluginAPI:TororoPluginAPI:$apiVersion")
+    compileOnly("com.github.tororo1066.TororoPluginAPI:CommandAPI:$apiVersion")
+    compileOnly("com.mojang:brigadier:1.0.18")
+}
+
+tasks.withType<ShadowJar> {
+    configurations = listOf(shadowImplementation)
+    archiveClassifier.set("")
+    exclude("kotlin/**")
+    exclude("org/intellij/lang/annotations/**")
+    exclude("org/jetbrains/annotations/**")
+
+    relocate("kotlin", "tororo1066.libs.kotlin")
+    relocate("org.intellij.lang.annotations", "tororo1066.libs.org.intellij.lang.annotations")
+    relocate("org.jetbrains.annotations", "tororo1066.libs.org.jetbrains.annotations")
+}
+
+tasks.named("build") {
+    dependsOn("shadowJar")
+}
+
+task<LaunchMinecraftServerTask>("buildAndLaunchServer") {
+    dependsOn("build")
+    doFirst {
+        copy {
+            from(buildDir.resolve("libs/${project.name}.jar"))
+            into(buildDir.resolve("MinecraftServer/plugins"))
+        }
+    }
+
+    jarUrl.set(JarUrl.Paper(pluginVersion))
+    jarName.set("server.jar")
+    serverDirectory.set(buildDir.resolve("MinecraftServer"))
+    nogui.set(true)
+    agreeEula.set(true)
+}
